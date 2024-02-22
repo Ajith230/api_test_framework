f3a10152ac4c03139d173e4e7445df2c947d7bf9
Ajith230 <ajithbabu9567@gmail.com>
2024-02-22 20:09:26 +0530

    simple_api_driver fixture removed

--- framework/driver_factory.py
+++ framework/driver_factory.py
@@ -1,16 +1,6 @@
 import logging
 
-import httpx
-import pytest
-
-import config
 from framework.driver import Driver
-
-
-@pytest.fixture
-def simple_api_driver():
-    driver = Driver(config.API_URL)
-    yield driver
 
 
 class SimpleApiDriverCopy:
